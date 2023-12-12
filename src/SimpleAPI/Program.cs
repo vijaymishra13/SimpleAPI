443b2e54f34c40395f19d3eb5902fd1f4062c4f7
Vijay Mishra <vijay.mishra13@gmail.com>
2023-12-12 00:18:08 -0500

    fixing typo in SimpleAPI project name

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Just adding a comment
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
